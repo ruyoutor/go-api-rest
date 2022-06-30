9f5c281f7c0110db4b5a69e5244dcb337dc0a8a0
Ruy Outor <ruy.outor@gmail.com>
2022-06-29 23:37:55 -0300

    inclusão de novos recursos

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -28,5 +28,31 @@ func GetPersonalityByID(w http.ResponseWriter, r *http.Request) {
 	var personality models.Personality
 	database.DB.Find(&personality, id)
 	json.NewEncoder(w).Encode(personality)
+}
+
+func CreateNewPersonality(w http.ResponseWriter, r *http.Request) {
+	var newPersonality models.Personality
+
+	json.NewDecoder(r.Body).Decode(&newPersonality)
+	database.DB.Create(&newPersonality)
+
+	json.NewEncoder(w).Encode(newPersonality)
+}
+
+func DeletePersonalityByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var personality models.Personality
+
+	database.DB.Delete(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func EditPersonality(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var personality models.Personality
+
+	database.DB.First(&personality, id)
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Save(&personality)
 
 }
--- routes/routes.go
+++ routes/routes.go
@@ -14,5 +14,8 @@ func HandleRequest() {
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.GetPersonalityByID).Methods("Get")
+	r.HandleFunc("/api/personalities/", controllers.CreateNewPersonality).Methods("Post")
+	r.HandleFunc("/api/personalities/{id}", controllers.DeletePersonalityByID).Methods("Delete")
+	r.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
