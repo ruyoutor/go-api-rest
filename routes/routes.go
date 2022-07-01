34fcd296407003cb0043d7bffbff8bd1cc10f0c7
Ruy Outor <ruy.outor@gmail.com>
2022-07-01 00:36:31 -0300

    final do curso, incluido front-end em react

--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/ruyoutor/go-api-rest/controllers"
+	"github.com/ruyoutor/go-api-rest/middleware"
 )
 
 func HandleRequest() {
 
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.GetPersonalityByID).Methods("Get")
 	r.HandleFunc("/api/personalities/", controllers.CreateNewPersonality).Methods("Post")
 	r.HandleFunc("/api/personalities/{id}", controllers.DeletePersonalityByID).Methods("Delete")
 	r.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
